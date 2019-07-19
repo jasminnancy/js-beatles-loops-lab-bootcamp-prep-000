b6fa53450fb1ecdc1a6369ec61e2324d3afb9a68
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-19 20:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array = []
+  var counter = 0
+  while (counter < facts.length){
+    array.push(facts[counter] + "!!!")
+    counter++
+  }
+  return array
+}
